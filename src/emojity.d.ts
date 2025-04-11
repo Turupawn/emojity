dc580cbb1a9929dff0be32f679afe8219047250c
Turupawn <ahmed.hn.43@gmail.com>
2025-04-11 12:39:42 -0600

    📦npm update

---
+++ src/emojity.d.ts
@@ -0,0 +1,17 @@
+declare function loadEmojiLib(): void;
+declare function loadOpcodeLib(): void;
+declare function compile(code: string): Promise<{
+    bytecode: string;
+    abi: string;
+    metadata: string;
+    solidityInterface: string;
+    sonatina: string;
+}>;
+
+declare const emojity: {
+    compile: typeof compile;
+    loadEmojiLib: typeof loadEmojiLib;
+    loadOpcodeLib: typeof loadOpcodeLib;
+};
+
+export = emojity; 
